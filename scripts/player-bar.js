c5a20b8eeeb3604ee507a70ce5012e99dedf7e15
Adam Essmyer <acemyer@Adams-MacBook-Pro.local>
2018-03-24 08:42:30 -0500

    added time and volume controls

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -14,6 +14,8 @@
     player.playPause(nextSong);
   });
 
+
+//previous button//
   $('button#previous').click('click',function() {
     if (player.playState !== 'playing') {return false};
 
@@ -22,4 +24,33 @@
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
+
+//time and volume//
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  $('#volume-control input').on('input', function (event){
+    player.setVolume(event.target.value);
+  });
+
+
+
+  setInterval(() => {
+
+    function updateTime()
+{
+  document.getElementById('current-time').innerHTML = [current-time].join('/')
+}
+setInterval(updateTime, 1000);
+
+    if(player.playState !== 'playing') { return true; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text(currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
+
+
 }
